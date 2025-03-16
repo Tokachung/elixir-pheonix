28d4b8de06a807f0ba76832c60df0a258855b4fa
tokachung <st1403@live.com>
2025-03-16 18:28:21 +1100

    test return json data

--- lib/forum_web/controllers/page_controller.ex
+++ lib/forum_web/controllers/page_controller.ex
@@ -6,4 +6,14 @@ defmodule ForumWeb.PageController do
     # so skip the default app layout.
     render(conn, :home, layout: false)
   end
+
+  def users(conn, _params) do
+    IO.puts("Users function hit!")
+    users = [
+      %{id: 1, name: "Alice", email: "alice@email.com"},
+      %{id: 1, name: "Bob", email: "alice@email.com"}
+    ]
+
+    json(conn, %{users: users})
+  end
 end
